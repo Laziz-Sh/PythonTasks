4cbc98fd8b1a5c3b870330c5cab57882fef2b3fe
Laziz-Sh <vaveligina@gmail.com>
2026-03-20 11:32:14 +0300

    LR4

---
+++ Лабораторная работа 4/lab4-2.py
@@ -0,0 +1,29 @@
+# TODO импортировать необходимые молули
+import csv
+import json
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+
+def task() -> None:
+    data = []
+    # TODO считать содержимое csv файла
+    with open(INPUT_FILENAME, encoding="utf-8") as f:
+        reader = csv.DictReader(f, delimiter=",")
+        for row in reader:
+            data.append(row)
+    with open(OUTPUT_FILENAME, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+
+
+# TODO Сериализовать в файл с отступами равными 4
+
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
